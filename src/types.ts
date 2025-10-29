83a0bc00d8b5ddf729ad54348af9e047460e0c94
Viktor Shymko <viktor.shymko@hebronsoft.com>
2025-10-29 12:16:12 +0200

    migrate to official mcp server lib

--- src/types.ts
+++ src/types.ts
@@ -18,26 +18,31 @@ export interface EvaluationResult {
 export interface ToolContent {
   type: 'text';
   text: string;
+  [key: string]: unknown;
 }
 
 export interface DebugScriptStructuredSuccess {
   results: EvaluationResult[];
+  [key: string]: unknown;
 }
 
 export interface DebugScriptStructuredError {
   error: string;
+  [key: string]: unknown;
 }
 
 export interface DebugScriptSuccess {
   content: ToolContent[];
   structuredContent: DebugScriptStructuredSuccess;
   isError?: false;
+  [key: string]: unknown;
 }
 
 export interface DebugScriptError {
   content: ToolContent[];
   structuredContent: DebugScriptStructuredError;
   isError: true;
+  [key: string]: unknown;
 }
 
 export type DebugScriptResponse = DebugScriptSuccess | DebugScriptError;
